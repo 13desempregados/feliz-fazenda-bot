1408573cb3a280c9e871cda44c1b885325f2989d
Diego Maia <diegoyt@unknowne66283898fbb.lan>
2025-06-22 21:00:35 +0100

    primeiro commit

---
+++ config.py
@@ -0,0 +1,16 @@
+# ========================
+# Feliz Fazenda Bot - config.py
+# ========================
+
+# TOKEN do seu bot
+TOKEN = '8076429583:AAHYd7fWYtEClThSLPP4WIptrr6sfGdGnq4'
+
+# API Key do NOWPayments
+NOWPAYMENTS_API_KEY = 'PXE0WY7-4VD4QR4-N4ZXP5Y-MD2XYCC'
+
+# Carteiras USDT
+USDT_TRC20 = 'TWJUc3fvD5RReUW1YgmsXfzAp8V26M2UuK'
+USDT_BEP20 = '0xDF0771a1f6EC68Ef10d10BE51e82D1E7749c8AF8'
+
+# ADMIN ID (o seu ID no Telegram para receber pedidos de saque — você pode colocar seu ID)
+ADMIN_ID = 000000000  # coloque aqui seu Telegram ID (depois te ensino como pegar)
